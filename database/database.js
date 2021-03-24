f79405c6315636104a46314c19c98f12afba3eab
BRSalva <murilosbagodi@hotmail.com>
2021-03-24 20:22:00 -0300

    quinto

--- database/database.js
+++ database/database.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize')
 
 const connection = new Sequelize('fesf1', 'uesc', 'Matrix22', {
-    
+
     host: 'mysql743.umbler.com',
     dialect: 'mysql'
-
 })
 
 module.exports = connection;
