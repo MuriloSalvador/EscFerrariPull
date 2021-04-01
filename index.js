74f302efe606d90111c6b92ca848a10a74c1172e
BRSalva <murilosbagodi@hotmail.com>
2021-04-01 11:51:23 -0300

    ERROR HTTPS REVOLVED

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-    res.redirect("https://escferrarisp-com-br.umbler.net" + req.url);
+    // res.redirect("https://escferrarisp-com-br.umbler.net" + req.url);
 
     // var etapa = 1
 
